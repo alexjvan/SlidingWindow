0a1b9ac3c45593bb5a17344d7d8434e8bf1a462f
MayKeziah <keziah.may@gmail.com>
2020-12-01 13:32:39 -0800

    Create kez_udphw3.cpp

---
+++ kez_udphw3.cpp
@@ -0,0 +1,31 @@
+#include<UdpSocket.h>
+/* ***************************************************************************
+ * UDPHW3.CPP
+ *  This assignment is a demonstration of understanding of two algorithms:
+ *  Stop-and-wait and Sliding-Window. 
+ * 
+ * Authors:   Alex Van Matre and Keziah May
+ * School:    University of Washington Bothell
+ * Course:    CSS432 Computer Networking
+ * Professor: Dr. Ahmed Awad
+ * 
+ * Sources:
+ *  
+ * ***************************************************************************/
+
+// Alex
+int clientStopWait(UdpSocket &sock, const int max, int message[]);
+void serverReliable(UdpSocket &sock, const int max, int message[]);
+
+
+// Me
+int clientSlidingWindow(UdpSocket &sock, const int max, int message[], int windowSize);
+void serverEarlyRetrans(UdpSocket &sock, const int max, int message[], int windowSize);
+
+int clientSlidingWindow(UdpSocket &sock, const int max, int message[], int windowSize) {
+
+}
+
+void serverEarlyRetrans(UdpSocket &sock, const int max, int message[], int windowSize) {
+
+}
